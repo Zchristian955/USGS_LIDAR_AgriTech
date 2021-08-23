b533092a33f7fdff2c67d9ddf0210e538a019149
Kevin Christian ZANOU <72617790+Zchristian955@users.noreply.github.com>
2021-08-23 08:34:54 +0200

    Update unitest.py

--- test/unitest.py
+++ test/unitest.py
@@ -1,12 +1,4 @@
 import unittest
 import sys, os
 
-class TestCountOccurence(unittest.TestCase):
-    def test_count_occurence(self):
-        """
-        Test that it returns the count of each unique values in the given list
-        """
-        data = [0,0,9,0,8,9,0,7]
-        result = count_occurence(data)
-        output = {0: 4, 9: 2, 8: 1, 7: 1}
-        self.assertAlmostEqual(result, output)
+
